cfbda221265b31b40fc172e3a69e31da7d0e631e
姚明飞 <yaomingfei@192.168.0.100>
2019-12-11 22:25:34 +0800

    1111

---
+++ src/views/ABC.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: yaomingfei
+ * @Date: 2019-12-11 22:00:50
+ * @LastEditors: yaomingfei
+ * @LastEditTime: 2019-12-11 22:01:18
+ -->
+<template>
+  <div class="about">
+    <h1>ABC</h1>
+  </div>
+</template>
