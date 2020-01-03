a61cdfb43f3d2764f5644e4918a950aaf871c056
chrisworkalx <857717575@qq.com>
2020-01-03 10:07:09 +0800

    2222

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: yaomingfei
+ * @Date: 2020-01-03 09:34:06
+ * @LastEditors  : yaomingfei
+ * @LastEditTime : 2020-01-03 10:05:55
+ -->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>This is an about page AAAA</h1>
   </div>
 </template>
